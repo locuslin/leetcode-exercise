a5dc4cfa62a4b9198b1af33093232242e2a8b0a1
locuslin <locus0@dingtalk.com>
2020-01-06 15:58:09 +0800

    完善头条的关于解码的动态规划的代码

--- src/main/java/me/locus0/ToutiaoInterview.java
+++ src/main/java/me/locus0/ToutiaoInterview.java
@@ -22,13 +22,42 @@ package me.locus0;
  */
 public class ToutiaoInterview {
 
-    public int getDecodeMethodsNum(String s) {
+    public int decodeNum(String s) {
 
-        // f(s) = f(s的子串)+1当前子串
+        // f(s) = f(s-1)+f(s-2)
+        // f 代表解法数量 -1 代表去除一个subString(1),-2 subString(2)
 
         //第一步 列举当前子串的取法
         // 要么取一个当做子串 要么取两个当子串
 
-        return 0;
+        //递归临界条件
+        char firstC = s.charAt(0);
+        if (firstC == '0') {
+            return 0;
+        }
+        if (s.length() == 1) {
+            //长度为1时 只要不是0 解法就为1
+            return 1;
+        } else if (s.length() == 2) {
+            // 长度为2
+            char secChar = s.charAt(1);
+            if (firstC == '2') {
+                if (secChar <= '6' && secChar > '0') {
+                    return 2;
+                } else {
+                    return 1;
+                }
+            } else if (firstC == '1') {
+                return 1 + decodeNum(s.substring(1));
+            } else {
+                //把首位去除之后，看末位是否能够满足
+                return decodeNum(s.substring(1));
+            }
+        } else {
+            return decodeNum(s.substring(0, 1)) * decodeNum(s.substring(1)) + decodeNum(s.substring(0, 2)) * decodeNum(
+                    s.substring(2));
+        }
+
     }
+
 }
